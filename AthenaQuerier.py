ac67be64efe29b98f4cbe8b70616c02d10f2fcba
p.huang <p.huang@house-of-communication.com>
2024-10-03 19:43:49 +0200

    Changed: Support multiple queries

---
+++ AthenaQuerier.py
@@ -0,0 +1,87 @@
+import time
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+
+# Ref: https://frankcorso.dev/querying-athena-python.html
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+class AthenaQuerier:
+    def __init__(self, database_name: str, output_location: str):
+        self.client = boto3.client('athena')  # Initialize Athena client
+        self.database = database_name
+        self.output_location = output_location
+        self.query_execution_id = None
+
+    def run_query(self, query: str):
+        try:
+            response = self.client.start_query_execution(
+                QueryString=query,
+                QueryExecutionContext={'Database': self.database},
+                ResultConfiguration={'OutputLocation': self.output_location}
+            )
+            return response['QueryExecutionId']
+        except ClientError as e:
+            logger.error(f"Error running query: {e}")
+            return None
+
+    def check_query_status(self, retries=10, wait_interval=2):
+        status = None
+        for attempt in range(retries):
+            try:
+                response = self.client.get_query_execution(QueryExecutionId=self.query_execution_id)
+                status = response['QueryExecution']['Status']['State']
+                if status in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
+                    break
+            except ClientError as e:
+                logger.error(f"Error checking query status: {e}")
+                if attempt < retries - 1:
+                    time.sleep(wait_interval)  # wait before retrying the request
+                else:
+                    raise
+
+            time.sleep(wait_interval)  # Wait for status
+
+        if status == 'FAILED':
+            raise Exception(f"Query failed: {response['QueryExecution']['Status']['StateChangeReason']}")
+        elif status == 'CANCELLED':
+            raise Exception("Query was cancelled.")
+        return status
+
+    def fetch_results(self):
+        try:
+            results = self.client.get_query_results(QueryExecutionId=self.query_execution_id)
+            return results['ResultSet']
+        except ClientError as e:
+            logger.error(f"Error fetching query results: {e}")
+            raise
+
+    def execute_athena_query(self, query: str):
+        self.query_execution_id = self.run_query(query)
+        logger.info(f"Query execution id: {self.query_execution_id}")
+
+        if self.query_execution_id:
+            status = self.check_query_status()
+            if status == 'SUCCEEDED':
+                logger.info(f"Query execution succeeded: {self.query_execution_id}")
+                query_results = self.fetch_results()
+                return query_results
+            else:
+                logger.error(f"Query did not succeed. Status: {status}")
+        else:
+            logger.error("Failed to start query execution.")
+        return None
+
+    def execute_multiple_athena_queries(self, query_list: list, wait_interval=2):
+        results = []
+        for query in query_list:
+            logger.info(f"Executing query: {query}")
+            result = self.execute_athena_query(query)
+            if result:
+                results.append(result)
+            time.sleep(wait_interval)  # to avoid throttling
+        return results
--- Query.py
+++ Query.py
@@ -1,98 +1,31 @@
-import boto3
-import time
 import logging
-from botocore.exceptions import ClientError
-
-# Ref: https://frankcorso.dev/querying-athena-python.html
-
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
-
-# Initialize Athena client
-client = boto3.client('athena')
-
-
-def run_query(query, database, output_location):
-    try:
-        response = client.start_query_execution(
-            QueryString=query,
-            QueryExecutionContext={'Database': database},
-            ResultConfiguration={'OutputLocation': output_location}
-        )
-        return response['QueryExecutionId']
-    except ClientError as e:
-        logger.error(f"Error running query: {e}")
-        return None
-
-
-def check_query_status(query_execution_id, retries=5):
-    status = None
-    for attempt in range(retries):
-        try:
-            response = client.get_query_execution(QueryExecutionId=query_execution_id)
-            status = response['QueryExecution']['Status']['State']
-            if status in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
-                break
-        except ClientError as e:
-            logger.error(f"Error checking query status: {e}")
-            if attempt < retries - 1:
-                time.sleep(5)  # wait for 5 seconds before retrying the request
-            else:
-                raise
-
-        time.sleep(2)  # Wait for status
-
-    if status == 'FAILED':
-        raise Exception(f"Query failed: {response['QueryExecution']['Status']['StateChangeReason']}")
-    elif status == 'CANCELLED':
-        raise Exception("Query was cancelled.")
-    return status
-
-
-def fetch_results(query_execution_id):
-    try:
-        results = client.get_query_results(QueryExecutionId=query_execution_id)
-        return results['ResultSet']
-    except ClientError as e:
-        logger.error(f"Error fetching query results: {e}")
-        raise
-
-
-def execute_athena_query(query, database, output_location):
-    # logger.info(f"Running query: {query}")
-    query_execution_id = run_query(query, database, output_location)
-    logger.info(f"Query execution id: {query_execution_id}")
-
-    if query_execution_id:
-        status = check_query_status(query_execution_id)
-        if status == 'SUCCEEDED':
-            logger.info(f"Query execution succeeded: {query_execution_id}")
-            query_results = fetch_results(query_execution_id)
-            return query_results
-        else:
-            logger.error(f"Query did not succeed. Status: {status}")
-    else:
-        logger.error("Failed to start query execution.")
-    return None
-
+import AthenaQuerier
 
 if __name__ == '__main__':
-    # Configuration for Athena query
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger()
+
     DATABASE_NAME = 'curated_dev_adform'
     OUTPUT_LOCATION = 's3://aws-athena-query-results-227956463654-eu-central-1/'
-    QUERY_STRING = '''
-    SELECT * 
-    FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic" 
-    ORDER BY 'date'
-    LIMIT 10;
-    '''
+    QUERY_LIST = [
+        '''
+        SELECT * 
+        FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic" 
+        ORDER BY date
+        LIMIT 10;
+        ''',
+        '''
+        SELECT COUNT(*) 
+        FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic";
+        '''
+    ]
 
     # Run the query
-    results = execute_athena_query(QUERY_STRING, DATABASE_NAME, OUTPUT_LOCATION)
+    querier = AthenaQuerier.AthenaQuerier(DATABASE_NAME, OUTPUT_LOCATION)
+    results = querier.execute_multiple_athena_queries(QUERY_LIST)
 
-    # Display the results
     if results:
-        logger.info("Query Results:")
-        for row in results['Rows']:
-            logger.info(row['Data'])
+        for result in results:
+            for row in result['Rows']:
+                logger.info(row['Data'])
