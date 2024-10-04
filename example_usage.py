446dd9af3317c0c1f50ecea25bffc4601b0ac08c
p.huang <p.huang@house-of-communication.com>
2024-10-04 09:12:24 +0200

    Changed: improved imports

--- example_usage.py
+++ example_usage.py
@@ -1,5 +1,5 @@
 import logging
-import athena_querier
+from athena_querier import AthenaQuerier
 
 if __name__ == '__main__':
     # Configure logging
@@ -10,22 +10,22 @@ if __name__ == '__main__':
     OUTPUT_LOCATION = 's3://aws-athena-query-results-227956463654-eu-central-1/'
     QUERY_LIST = [
         '''
-        SELECT * 
-        FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic" 
+        SELECT *
+        FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic"
         ORDER BY date
         LIMIT 10;
         ''',
         '''
-        SELECT COUNT(*) 
+        SELECT COUNT(*)
         FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic";
         '''
     ]
 
     # Run the query
-    querier = athena_querier.AthenaQuerier(DATABASE_NAME, OUTPUT_LOCATION)
+    querier = AthenaQuerier(DATABASE_NAME, OUTPUT_LOCATION)
     results = querier.execute_multiple_athena_queries(QUERY_LIST)
 
-    # # Debugging
+    # Debugging
     # if results:
     #     for result in results:
     #         for row in result['Rows']:
