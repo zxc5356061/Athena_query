9adfed4ed3db9cf3164e915c238ceafc2d6895d6
p.huang <p.huang@house-of-communication.com>
2024-10-09 11:49:33 +0200

    Changed: Support read queries from .sql file

---
+++ campaign_performance_query.sql
@@ -0,0 +1,5 @@
+
+SELECT *
+FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic"
+ORDER BY date
+LIMIT 10;
--- example_usage.py
+++ example_usage.py
@@ -1,32 +1,26 @@
 import logging
 from athena_querier import AthenaQuerier
 
-if __name__ == '__main__':
+def to_run():
     # Configure logging
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger()
 
-    DATABASE_NAME = 'curated_dev_adform'
-    OUTPUT_LOCATION = 's3://aws-athena-query-results-227956463654-eu-central-1/'
-    QUERY_LIST = [
-        '''
-        SELECT *
-        FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic"
-        ORDER BY date
-        LIMIT 10;
-        ''',
-        '''
-        SELECT COUNT(*)
-        FROM "curated_dev_adform"."dev_campaign_performance_daily_mediaplus_czech_republic";
-        '''
-    ]
+    file_path = '/Users/huangp/Documents/Data_team/DT_project/Athena_query/campaign_performance_query.sql'
+    database_name = 'curated_dev_adform'
+    output_location = 's3://aws-athena-query-results-227956463654-eu-central-1/'
 
-    # Run the query
-    querier = AthenaQuerier(DATABASE_NAME, OUTPUT_LOCATION)
-    results = querier.execute_multiple_athena_queries(QUERY_LIST)
+    with open(file_path, 'r') as QUERY:
+        # Run the query
+        querier = AthenaQuerier(database_name, output_location)
+        # results = querier.execute_multiple_athena_queries(QUERY_LIST)
+        results = querier.execute_athena_query(QUERY.read())
 
-    # Debugging
-    # if results:
-    #     for result in results:
-    #         for row in result['Rows']:
-    #             logger.info(row['Data'])
+        # Debugging
+        if results:
+            for row in results['Rows']:
+                logger.info(row['Data'])
+
+
+if __name__ == '__main__':
+    to_run()
